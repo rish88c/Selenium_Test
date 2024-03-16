c3ddb1f0bea9c47d7bebae25a4bf09b9d71155fb
Rishabh Gupta <113669945+rish88c@users.noreply.github.com>
2024-03-16 19:46:53 +0530

    Update SeleniumTest.java

--- src/test/com/autoamtiononthego/SeleniumTest.java
+++ src/test/com/autoamtiononthego/SeleniumTest.java
@@ -1,4 +1,4 @@
-package com.autoamtiononthego;
+package com.automationonthego;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
@@ -19,58 +19,42 @@ public class SeleniumTest {
     WebDriver driver;
     String appUrl = "https://admin-demo.virtocommerce.com/#/login";
 
-    @Parameters({ "browser" })
+    @Parameters({"browser"})
     @BeforeClass
     public void setUp(String browser) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            // Set Chrome driver executable path
+        if (browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("localchrome")) {
             System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedrivernew.exe");
-            // Initialize ChromeDriver with options
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--start-maximized");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
-            // You can add Firefox setup here if needed
-        } else if (browser.equalsIgnoreCase("localchrome")) {
-            // Initialize local ChromeDriver
-            System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedrivernew.exe");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+            // Add Firefox setup here if needed
         } else {
             throw new IllegalArgumentException("Invalid browser specified: " + browser);
         }
-        // Open the application URL
         driver.get(appUrl);
     }
 
     @Test(priority = 0)
     public void testLoginWithInvalidCredentials() {
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        // Find login element and enter invalid credentials
         WebElement loginInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
         loginInput.sendKeys("Demo");
         driver.findElement(By.name("password")).sendKeys("demo");
         driver.findElement(By.xpath("//button[text()='Log in']")).click();
-        // Verify error message
-        WebElement errorMessage = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='form-error']")));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='form-error']")));
         Assert.assertEquals(errorMessage.getText().trim(), "The login or password is incorrect.");
-        // Refresh the page
         driver.navigate().refresh();
     }
 
     @Test(priority = 1)
     public void testLoginWithValidCredentials() {
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        // Find login element and enter valid credentials
         WebElement loginInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
         loginInput.sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("store");
         driver.findElement(By.xpath("//button[text()='Log in']")).click();
-        // Wait for the 'New password' label to appear
-        WebElement newPasswordLabel = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(text(),'New password:')]")));
-        // Verify the presence of 'New password' label
+        WebElement newPasswordLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(text(),'New password:')]")));
         Assert.assertTrue(newPasswordLabel.isDisplayed(), "'New password' label is not displayed.");
     }
 
@@ -81,7 +65,6 @@ public class SeleniumTest {
 
     @AfterClass
     public void tearDown() {
-        // Quit the WebDriver instance
         if (driver != null) {
             driver.quit();
         }
@@ -91,11 +74,9 @@ public class SeleniumTest {
     public void captureScreenshot(ITestResult result) {
         if (result.getStatus() == ITestResult.FAILURE) {
             try {
-                // Capture screenshot on test failure
                 TakesScreenshot ts = (TakesScreenshot) driver;
                 File source = ts.getScreenshotAs(OutputType.FILE);
-                String screenshotPath = Paths.get("").toAbsolutePath().toString() + "/Screenshots/" + result.getName()
-                        + ".png";
+                String screenshotPath = Paths.get("").toAbsolutePath().toString() + "/Screenshots/" + result.getName() + ".png";
                 File target = new File(screenshotPath);
                 FileUtils.copyFile(source, target);
                 System.out.println("Screenshot captured at: " + screenshotPath);
